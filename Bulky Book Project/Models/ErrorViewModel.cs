c5a109fbdb4b751759888ce80256338db01b0582
sarathchandra <sarathchandra265@gmail.com>
2020-08-23 13:38:35 +0530

    Add project files.

---
+++ Bulky Book Project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Bulky_Book_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
